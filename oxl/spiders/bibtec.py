7ca13ca5f37ccb1d332688469b66565d80f6eb40
César Bravo <cebravo94@gmail.com>
2018-09-20 18:42:28 -0300

    Bibtec downloader working

--- oxl/spiders/bibtec.py
+++ oxl/spiders/bibtec.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import time
 import scrapy
+import urllib
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+
 class BibtecSpider(CrawlSpider):
     name = "bibtec"
     allowed_domains = ["dblp.uni-trier.de"]
@@ -11,10 +14,23 @@ class BibtecSpider(CrawlSpider):
     ]
 
     rules = {
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul/li/a')), callback="parse_item", follow=True),
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//li[@class="export drop-down"]/div[@class="body"]/ul/li[last()]')), callback="parse_item", follow=True)
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul/li/a'))),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//li[@class="export drop-down"]/div[@class="body"]/ul/li[last()]'))),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul//li[2]/div[2]/ul[1]/li[1]/a'))),
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/p/a')), 
+            callback="parse_item", 
+            follow=False)
     }
 
     def parse_item(self, response):
-        time.sleep(1)
-        print('Processing..' + response.url)
+        time.sleep(5)
+        url = response.url+""
+        if (url.endswith(".bib")):
+            print('Bibtec URL.. ' + url) #link de descarga
+            filename = url.rsplit('/',1)[1]
+            print(filename)
+            urllib.urlretrieve (url, filename) #descarga
+        
+
+        #//*[@id="main"]/p/a                                -- bibtec juntos
+        #//*[@id="main"]/ul//li[2]/div[2]/ul[1]/li[1]/a     -- bibtec separados

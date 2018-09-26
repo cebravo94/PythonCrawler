3daec02b4c827ef2e556a5262887525c2c3cdcad
César Bravo <cebravo94@gmail.com>
2018-09-26 01:13:08 -0300

    Cambios para adaptarlo al proyecto

--- oxl/spiders/bibtec.py
+++ oxl/spiders/bibtec.py
@@ -4,6 +4,7 @@ import scrapy
 import urllib
 
 from scrapy.spiders import CrawlSpider, Rule
+from scrapy.selector import HtmlXPathSelector
 from scrapy.linkextractors import LinkExtractor
 
 class BibtecSpider(CrawlSpider):
@@ -14,22 +15,46 @@ class BibtecSpider(CrawlSpider):
     ]
 
     rules = {
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul/li/a'))),                         #xpaths de los menus
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul//li[2]/div[2]/ul[1]/li[1]/a')),   #xpaths de los enlace a las bibtec
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul/li/a')),   #xpaths de los menus
             callback="parse_item", 
-            follow=False)
+            follow=True)
         
     }
 
     def parse_item(self, response):
-        url = str(response.xpath('//*[@id="main"]/p/a').extract())  #xpath del enlace a descarga
-        url = url.split(' ')[1]
-        url = url.split('"')[1]
-        
-        if (url.endswith(".bib")):
-            print('Bibtec URL.. ' + url) #link de descarga
-            filename = url.rsplit('/',1)[1]
-            print(filename)
-            urllib.urlretrieve (url, filename) #descarga
-        
-        time.sleep(5)
+        url = (str)(response.url)
+        n = len(url.split("/"))
+        contiene = "publ" in url
+        if (n==7 and url.endswith("index.html")==False and contiene==False):
+            hxs = HtmlXPathSelector(response)
+            vols = hxs.select('//*[@id="main"]/ul')
+            n = len(vols)
+            for i in range(1,n+1):
+                paths = '//*[@id="main"]/ul['+str(i)+']/li'
+                pubs = hxs.select(paths)
+                o  = len(pubs)
+                for j in range(1, o+1):
+                    pathbibtec = '//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']/nav/ul/li[2]/div/ul/li[1]/a'
+                    pathauthors = '//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']/div[2]/span/a'
+                    urlbib = (str)(response.xpath(pathbibtec).extract())
+                    urlbib = urlbib.split('"')[1]
+                    f = urllib.urlopen(urlbib)
+                    s = f.read()
+                    s = s.split('<pre class="verbatim select-on-click">')[1]
+                    s = s.split('</pre>')[0]
+                    s = s.encode('utf8')
+                    print(s)
+
+                    s = (str)(response.xpath(pathauthors).extract())
+                    s2 = s.split("href")
+                    p=len(s2)
+                    authors = []
+                    for k in range(0,p):
+                        s3 = s2[k]
+                        if '"' in s3:
+                            s3 = s3.split('"')[1]
+                            authors.append(s3)
+                    for k in range(0, len(authors)):
+                        print(authors[k])
+
+            time.sleep(1)

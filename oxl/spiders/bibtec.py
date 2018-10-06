7c75f63e909cf9f03ab5b68a1c0e9c9ab0c802b7
César Bravo <cebravo94@gmail.com>
2018-10-06 01:03:12 -0300

    se leen páginas desde el índice de autores

--- oxl/spiders/bibtec.py
+++ oxl/spiders/bibtec.py
@@ -13,33 +13,40 @@ from pybtex.database import parse_string
 
 class BibtecSpider(CrawlSpider):
     name = "bibtec"
-    allowed_domains = ["dblp.uni-trier.de"]
+    allowed_domains = ["dblp.uni-trier.de","dblp.org"]
     start_urls = [
         #'https://dblp.uni-trier.de/db/journals/publ/',
-        #
-        'https://dblp.uni-trier.de/db/conf/',
-        'https://dblp.org/db/journals/'
-        #'https://dblp.org/db/journals/corr/'
+        #'https://dblp.uni-trier.de/db/conf/',
+        #'https://dblp.org/db/journals/'
+        'https://dblp.org/pers'
     ]
+    dont_filter=True
 
     rules = {
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul/li/a')),   #xpaths de los menus
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="main"]/ul/li/a')),   #xpaths de los menus
+        #    callback="parse_item", 
+        #    follow=True),
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="browse-conf-output"]/div/ul/li')),   #xpaths de los menus
+        #    callback="parse_item", 
+        #    follow=True),
+        #Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="browse-conf-output"]/p[2]/a[2]'))) #boton next para conferencias
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="browse-person-output"]/div/div/ul/li')),   #xpaths de los menus de personas
             callback="parse_item", 
             follow=True),
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="browse-conf-output"]/div/ul/li')),   #xpaths de los menus
-            callback="parse_item", 
-            follow=True),
-        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="browse-conf-output"]/p[2]/a[2]'))) #xpath next
+        Rule(LinkExtractor(allow=(), restrict_xpaths=('//*[@id="browse-person-output"]/p[1]/a[2]'))) #boton next para personas
         
     }
 
     def parse_item(self, response):
+
         url = (str)(response.url)
-        n = len(url.split("/"))
+        print (url)
+        #print (response.xpath('//*[@id="browse-person-output"]/header/h2').extract())
         try:
             print("\nSe intenta parsear "+response.url)
             hxs = HtmlXPathSelector(response)
-            vols = hxs.select('//*[@id="main"]/ul')
+            bloques = '//*[@id="publ-section"]'
+            nbloques = hxs.select(bloques)
             #journalurl = (str)(response.xpath('//*[@id="breadcrumbs"]/ul/li/span[3]/a').extract())
             #journalurl = journalurl.split('"')[1]
             #journal = (str)(response.xpath('//*[@id="breadcrumbs"]/ul/li/span[3]/a/span').extract())
@@ -47,66 +54,68 @@ class BibtecSpider(CrawlSpider):
             #journal = journal.split("<")[0]
             #print("El journal es "+journal)
             #print("El url del journal es "+journalurl)
-            n = len(vols)
+            n = len(nbloques)
             for i in range(1,n+1):
-                paths = '//*[@id="main"]/ul['+str(i)+']/li'
+                paths = bloques+'/div['+str(i)+']/div/ul/li'
                 print("Se parsea el bloque "+paths)
                 pubs = hxs.select(paths)
                 o  = len(pubs)
                 for j in range(1, o+1):
                     print("")
-                    print("Se parsea el bloque "+'//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']')
-                    titulo = (str)(response.xpath('//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']/div[2]/span[@class="title"]').extract())
-                    titulo = titulo.split(">")[1]
-                    titulo = titulo.split("<")[0]
-                    publicacionurl = (str)(response.xpath('//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']/nav/ul/li[4]/div[2]/ul[2]/li/small').extract())
-                    publicacionurl = publicacionurl.split(">")[1]
-                    publicacionurl = publicacionurl.split("<")[0]
-                    print("El título es "+titulo)
-                    print("El url del artículo es "+publicacionurl)
-                    pathbibtec = '//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']/nav/ul/li[2]/div/ul/li[1]/a'
-                    pathauthors = '//*[@id="main"]/ul['+str(i)+']/li['+str(j)+']/div[2]/span/a'
-                    urlbib = (str)(response.xpath(pathbibtec).extract())
-                    urlbib = urlbib.split('"')[1]
-
                     try:
-                        s = (str)(response.xpath(pathauthors).extract()) #Obtiene las url de los autores
-                        s2 = s.split("href")
-                        p=len(s2)
-                        authors = []
-                        for k in range(0,p):
-                            s3 = s2[k]
-                            if '"' in s3:
-                                s3 = s3.split('"')[1]
-                                authors.append(s3)
-                        print ("Autores")
-                        for k in range(0, len(authors)):
-                            print(authors[k])
-                    except:
-                        print ("No hay autores")
+                        print("Se parsea el bloque "+paths+'['+str(j)+']')
+                        titulo = (str)(response.xpath(paths+'['+str(j)+']/div/span[@class="title"]').extract())
+                        titulo = titulo.split(">")[1]
+                        titulo = titulo.split("<")[0]
+                        #publicacionurl = (str)(response.xpath(paths+'['+str(j)+']/nav/ul/li[4]/div[2]/ul[2]/li/small').extract())
+                        #publicacionurl = publicacionurl.split(">")[1]
+                        #publicacionurl = publicacionurl.split("<")[0]
+                        print("El título es "+titulo)
+                        #print("El url del artículo es "+publicacionurl)
+                        pathbibtec = paths+'['+str(j)+']/nav/ul/li[2]/div/ul/li[1]/a'
+                        pathauthors = paths+'['+str(j)+']/div[2]/span/a'
+                        urlbib = (str)(response.xpath(pathbibtec).extract())
+                        urlbib = urlbib.split('"')[1]
+                        try:
+                            s = (str)(response.xpath(pathauthors).extract()) #Obtiene las url de los autores
+                            s2 = s.split("href")
+                            p=len(s2)
+                            authors = []
+                            for k in range(0,p):
+                                s3 = s2[k]
+                                if '"' in s3:
+                                    s3 = s3.split('"')[1]
+                                    authors.append(s3)
+                            print ("Autores")
+                            for k in range(0, len(authors)):
+                                print(authors[k])
+                        except:
+                            print ("No hay autores")
 
-                    try:
-                        print("La URL del bibtec es: "+urlbib)
-                        
-                        f = urllib.urlopen(urlbib) #Abre la URL del bibtec
-                        s = f.read()
-                        s = s.split('<pre class="verbatim select-on-click">')[1]
-                        s = s.split('</pre>')[0]
-                        s = s.replace("\n","")
-                        s = re.sub("\s\s+", " ", s)
-                        #tipo = s.split("@")[0]
-                        tipo = s.split("{")[0]
-                        tipo = tipo.replace("@","")
-                        print tipo
-                        bib_data = parse_string(s,"bibtex")
-                        for entry in bib_data.entries.values():
-                            #print entry.key
-                            #print entry.fields.keys()
-                            for value in entry.fields.keys():
-                                print value ,"=", bib_data.entries[entry.key].fields[value]
-
+                        try:
+                            print("La URL del bibtec es: "+urlbib)
+                            
+                            f = urllib.urlopen(urlbib) #Abre la URL del bibtec
+                            s = f.read()
+                            s = s.split('<pre class="verbatim select-on-click">')[1]
+                            s = s.split('</pre>')[0]
+                            s = s.replace("\n","")
+                            s = re.sub("\s\s+", " ", s)
+                            #tipo = s.split("@")[0]
+                            tipo = s.split("{")[0]
+                            tipo = tipo.replace("@","")
+                            print tipo
+                            bib_data = parse_string(s,"bibtex")
+                            for entry in bib_data.entries.values():
+                                #print entry.key
+                                #print entry.fields.keys()
+                                for value in entry.fields.keys():
+                                    print value ,"=", bib_data.entries[entry.key].fields[value]
+                        except:
+                            print("No se pudo parsear el bibtec")
                     except:
-                        print("No se pudo parsear el bibtec")
+                        print ("No es un bloque valido")
+
                     
                     print ("Publicación parseada con exito")
                 print ("Bloque parseado con exito")
